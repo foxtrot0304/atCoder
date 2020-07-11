9ab1c835ed261f3d82414eda8752923a89343ada
foxtrot0304 <git@lifeissunnyday.com>
2020-07-11 23:38:42 +0900

    improve template

--- template.java
+++ template.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    String str = scan.nextLine();
+    String str_scan = scan.nextLine();
+    // スペース区切りで分ける
+    var str = str_scan.split(" ");
+    // int型へ変換
+    var ans = Integer.parseInt(str_scan);
 
-    System.out.println(str);
+    System.out.println(ans);
     scan.close();
   }
 }
