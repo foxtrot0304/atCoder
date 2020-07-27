60d39c1572df62cd5d200a7894c1f5b1c65b85c2
foxtrot0304 <git@lifeissunnyday.com>
2020-07-28 01:24:09 +0900

    resolved B firsttime.

---
+++ M-SOLUTION_20200725/B_Magic2/B_Magic2.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String str_scan = scan.nextLine();
+    // スペース区切りで分ける
+    var str = str_scan.split(" ");
+    // int型へ変換
+    var A = Integer.parseInt(str[0]);
+    var B = Integer.parseInt(str[1]);
+    var C = Integer.parseInt(str[2]);
+
+    String ans = "no";
+
+    String k_scan = scan.nextLine();
+    var K = Integer.parseInt(k_scan);
+
+    for (var i=0;i<K;i++) {
+      var Atmp = A << i;
+      for (var j=0;j<K;j++) {
+        var Btmp = B << j;
+        for (var f=0;f<K;f++) {
+          var Ctmp = C << f;
+
+          if ( (Btmp > Atmp)  && (Ctmp > Btmp) ) {
+            ans = "yes";
+            break;
+          }
+
+        }
+        if (ans == "yes") {
+          break;
+        }
+      }
+      if (ans == "yes") {
+        break;
+      }
+    }
+
+    System.out.println(ans);
+    scan.close();
+  }
+}
