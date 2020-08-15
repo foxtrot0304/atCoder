a2db5af8c07bf93dadb93d1b8ee8f2cf340f4440
foxtrot0304 <git@lifeissunnyday.com>
2020-08-15 22:54:26 +0900

    resolved C

--- ABC175/C_WalkingTakahashi/walkingtakahashi.java
+++ ABC175/C_WalkingTakahashi/walkingtakahashi.java
@@ -12,23 +12,31 @@ public class Main {
     var D = Long.parseLong(str[2]);
 
     long ans = 0;
+    long syou = 0;
+    long amari = 0;
 
     if ( X < 0 ){
       X = X * -1;
     }
 
-    for (var i=0;i < K;i++){
-      if ( X > 0 ) {
-        X = X - D;
-      } else {
-        X = X + D;
-      }
-    }
+    syou = X / D;
+    amari = X % D;
 
-    if (X < 0) {
-      ans = -1 * X;
+    if ( syou < K ) {
+      if ((K % 2 == 1) && (syou % 2 == 1)){
+        ans = amari;
+      } else if ( (K % 2 == 0) && (syou % 2 == 0)) {
+        ans = amari;
+      } else if ( (K % 2 == 0) && (syou % 2 == 1)) {
+          ans = amari - D;
+      } else if ( (K % 2 == 1) && (syou % 2 == 0)){
+          ans = amari - D;
+      }
+      if (ans < 0) {
+        ans = -1 * ans;
+      }
     } else {
-      ans = X;
+      ans = amari + ((syou-K) * D);
     }
 
     System.out.println(ans);
