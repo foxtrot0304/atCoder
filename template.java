33410fe0cfaf5ded85926c80d51973b9fde70d0e
foxtrot0304 <git@lifeissunnyday.com>
2020-09-24 01:17:11 +0900

    add to template

--- template.java
+++ template.java
@@ -9,6 +9,12 @@ public class Main {
     // int型へ変換
     var ans = Integer.parseInt(str_scan);
 
+    // 連続したN個の行列を取り出すとき
+    String[] D = new String[N];
+    for(var i=0;i < N;i++){
+      D[j] = scan.nextLine();
+    }
+
     System.out.println(ans);
     scan.close();
   }
